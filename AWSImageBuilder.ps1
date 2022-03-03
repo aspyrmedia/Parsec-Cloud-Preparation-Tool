35e27cccfaa074ea92fea4f29acd1a6fa50cea05
Daniel Hagen <daniel.b.hagen@gmail.com>
2022-03-02 22:22:25 -0600

    Create AWSImageBuilder.ps1
    Simplifying scripts for AWS Image Builder Use.

---
+++ AWSImageBuilder.ps1
@@ -0,0 +1,278 @@
+
+# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+# Install-Module -Name 'Carbon' -AllowClobber -Force -Confirm:$false
+# Import-Module 'Carbon'
+Function Test-RegistryValue {
+  param(
+    [Alias("PSPath")]
+    [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [String]$Path
+    ,
+    [Parameter(Position = 1, Mandatory = $true)]
+    [String]$Value
+    ,
+    [Switch]$PassThru
+  ) 
+
+  process {
+    if (Test-Path $Path) {
+      $Key = Get-Item -LiteralPath $Path
+      if ($Key.GetValue($Value, $null) -ne $null) {
+        if ($PassThru) {
+          Get-ItemProperty $Path $Value
+        }
+        else {
+          $true
+        }
+      }
+      else {
+        $false
+      }
+    }
+    else {
+      $false
+    }
+  }
+}
+
+$path = [Environment]::GetFolderPath("Desktop")
+if ((Test-Path -Path $path\ParsecTemp ) -eq $true) {
+} 
+Else {
+  New-Item -Path $path\ParsecTemp -ItemType directory | Out-Null
+}
+Unblock-File -Path .\*
+copy-Item .\* -Destination $path\ParsecTemp\ -Force -Recurse | Out-Null
+Start-Sleep -s 1
+Get-ChildItem -Path $path\ParsecTemp -Recurse | Unblock-File
+$path = [Environment]::GetFolderPath("Desktop")
+$currentusersid = Get-LocalUser "$env:USERNAME" | Select-Object SID | ft -HideTableHeaders | Out-String | ForEach-Object { $_.Trim() }
+
+if ((Test-Path -Path $env:ProgramData\ParsecLoader) -eq $true) {} Else { New-Item -Path $env:ProgramData\ParsecLoader -ItemType directory | Out-Null }
+if ((Test-Path $env:ProgramData\ParsecLoader\parsecpublic.cer) -eq $true) {} Else { Move-Item -Path $path\ParsecTemp\PreInstall\parsecpublic.cer -Destination $env:ProgramData\ParsecLoader }
+
+# Create ParsecTemp folder in C Drive
+if ((Test-Path -Path C:\ParsecTemp) -eq $true) {} Else { New-Item -Path C:\ParsecTemp -ItemType directory | Out-Null }
+if ((Test-Path -Path C:\ParsecTemp\Apps) -eq $true) {} Else { New-Item -Path C:\ParsecTemp\Apps -ItemType directory | Out-Null }
+if ((Test-Path $env:ProgramData\ParsecLoader\TeamMachineSetup.ps1) -eq $true) {} Else { Move-Item -Path $path\ParsecTemp\PreInstall\TeamMachineSetup.ps1 -Destination $env:ProgramData\ParsecLoader }
+if ((Test-Path -Path C:\ParsecTemp\Drivers) -eq $true) {} Else { New-Item -Path C:\ParsecTemp\Drivers -ItemType Directory | Out-Null }
+
+# Disable IE Security
+Set-Itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -name IsInstalled -value 0 -force | Out-Null
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
+Stop-Process -Name Explorer -Force
+
+#download-files-S3
+(New-Object System.Net.WebClient).DownloadFile("https://builds.parsecgaming.com/package/parsec-windows.exe", "C:\ParsecTemp\Apps\parsec-windows.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://builds.parsec.app/vdd/parsec-vdd-0.37.0.0.exe", "C:\ParsecTemp\Apps\parsec-vdd.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPUUpdaterTool.ps1", "$env:ProgramData\ParsecLoader\GPUUpdaterTool.ps1")
+Unblock-File -Path "$env:ProgramData\ParsecLoader\GPUUpdaterTool.ps1"
+
+Install-WindowsFeature Direct-Play | Out-Null
+Install-WindowsFeature Net-Framework-Core | Out-Null
+
+#Sets all applications to force close on shutdown
+if (((Get-Item -Path "HKCU:\Control Panel\Desktop").GetValue("AutoEndTasks") -ne $null) -eq $true) {
+  Set-ItemProperty -path "HKCU:\Control Panel\Desktop" -Name "AutoEndTasks" -Value "1"
+}
+Else {
+  New-ItemProperty -path "HKCU:\Control Panel\Desktop" -Name "AutoEndTasks" -Value "1"
+}
+
+#disable new network Public/Private window, default Public
+if ((Test-RegistryValue -path HKLM:\SYSTEM\CurrentControlSet\Control\Network -Value NewNetworkWindowOff) -eq $true) {} Else { new-itemproperty -path HKLM:\SYSTEM\CurrentControlSet\Control\Network -name "NewNetworkWindowOff" | Out-Null }
+
+#disable logout start menu
+if ((Test-RegistryValue -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Value StartMenuLogOff ) -eq $true) { Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name StartMenuLogOff -Value 1 | Out-Null } Else { New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name StartMenuLogOff -Value 1 | Out-Null }
+
+#disable lock start menu
+if ((Test-Path -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System) -eq $true) {} Else { New-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies -Name Software | Out-Null }
+if ((Test-RegistryValue -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Value DisableLockWorkstation) -eq $true) { Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name DisableLockWorkstation -Value 1 | Out-Null } Else { New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name DisableLockWorkstation -Value 1 | Out-Null }
+
+#show hidden items
+set-itemproperty -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name Hidden -Value 1 | Out-Null
+
+#show file extensions
+Set-itemproperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -name HideFileExt -Value 0 | Out-Null
+
+#Enable Pointer Precision 
+Set-Itemproperty -Path 'HKCU:\Control Panel\Mouse' -Name MouseSpeed -Value 1 | Out-Null
+
+#enable Mouse Keys
+set-Itemproperty -Path 'HKCU:\Control Panel\Accessibility\MouseKeys' -Name Flags -Value 63 | Out-Null
+
+#set automatic time and timezone
+Set-ItemProperty -path HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters -Name Type -Value NTP | Out-Null
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate -Name Start -Value 00000003 | Out-Null
+
+#Disables Server Manager opening on Startup
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask | Out-Null
+
+# Install Parsec Agent
+Start-Process "C:\ParsecTemp\Apps\parsec-windows.exe" -ArgumentList "/silent", "/shared" -wait
+Start-Sleep -s 1
+Start-Process -FilePath "C:\Program Files\Parsec\parsecd.exe"
+Start-Sleep -s 1
+
+#Disable Devices
+Start-Process -FilePath "C:\Program Files\Parsec\vigem\10\x64\devcon.exe" -ArgumentList '/r disable "HDAUDIO\FUNC_01&VEN_10DE&DEV_0083&SUBSYS_10DE11A3*"'
+Get-PnpDevice | where { $_.friendlyname -like "Generic Non-PNP Monitor" -and $_.status -eq "OK" } | Disable-PnpDevice -confirm:$false
+Get-PnpDevice | where { $_.friendlyname -like "Microsoft Basic Display Adapter" -and $_.status -eq "OK" } | Disable-PnpDevice -confirm:$false
+Get-PnpDevice | where { $_.friendlyname -like "Google Graphics Array (GGA)" -and $_.status -eq "OK" } | Disable-PnpDevice -confirm:$false
+Get-PnpDevice | where { $_.friendlyname -like "Microsoft Hyper-V Video" -and $_.status -eq "OK" } | Disable-PnpDevice -confirm:$false
+Start-Process -FilePath "C:\Program Files\Parsec\vigem\10\x64\devcon.exe" -ArgumentList '/r disable "PCI\VEN_1013&DEV_00B8*"'
+Start-Process -FilePath "C:\Program Files\Parsec\vigem\10\x64\devcon.exe" -ArgumentList '/r disable "PCI\VEN_1D0F&DEV_1111*"'
+Start-Process -FilePath "C:\Program Files\Parsec\vigem\10\x64\devcon.exe" -ArgumentList '/r disable "PCI\VEN_1AE0&DEV_A002*"'
+
+# Install Parsec Virtual Display driver
+Import-Certificate -CertStoreLocation "Cert:\LocalMachine\TrustedPublisher" -FilePath "$env:ProgramData\ParsecLoader\parsecpublic.cer" | Out-Null
+Start-Process "C:\ParsecTemp\Apps\parsec-vdd.exe" -ArgumentList "/silent" 
+$iterator = 0    
+do {
+  Start-Sleep -s 2
+  $iterator++
+}
+Until (($null -ne ((Get-PnpDevice | Where-Object { $_.Name -eq "Parsec Virtual Display Adapter" }).DeviceID)) -or ($iterator -gt 7))
+if (Get-process -name parsec-vdd -ErrorAction SilentlyContinue) {
+  Stop-Process -name parsec-vdd -Force
+}
+$configfile = Get-Content C:\ProgramData\Parsec\config.txt
+$configfile += "host_virtual_monitors = 1"
+$configfile += "host_privacy_mode = 1"
+$configfile | Out-File C:\ProgramData\Parsec\config.txt -Encoding ascii
+
+# Install XBox 360 Controller driver in Windows Server 2019
+if ((gwmi win32_operatingsystem | % caption) -like '*Windows Server 2019*') {
+        (New-Object System.Net.WebClient).DownloadFile("http://www.download.windowsupdate.com/msdownload/update/v3-19990518/cabpool/2060_8edb3031ef495d4e4247e51dcb11bef24d2c4da7.cab", "C:\ParsecTemp\Drivers\Xbox360_64Eng.cab")
+  if ((Test-Path -Path C:\ParsecTemp\Drivers\Xbox360_64Eng) -eq $true) {} Else { New-Item -Path C:\ParsecTemp\Drivers\Xbox360_64Eng -ItemType directory | Out-Null }
+  cmd.exe /c "C:\Windows\System32\expand.exe C:\ParsecTemp\Drivers\Xbox360_64Eng.cab -F:* C:\ParsecTemp\Drivers\Xbox360_64Eng" | Out-Null
+  cmd.exe /c '"C:\Program Files\Parsec\vigem\10\x64\devcon.exe" dp_add "C:\ParsecTemp\Drivers\Xbox360_64Eng\xusb21.inf"' | Out-Null
+}
+
+# Installing VB Cable Audio Driver if required
+# $gputype = Get-PnpDevice | Where-Object { ($_.DeviceID -like 'PCI\VEN_10DE*' -or $_.DeviceID -like '*PCI\VEN_1002*') -and ($_.PNPClass -eq 'Display' -or $_.Name -like '*Video Controller') } | Select-Object InstanceID -ExpandProperty InstanceID
+# if ($gputype -eq $null) {
+# }
+# Else {
+#   if ($gputype.substring(13, 8) -eq "DEV_13F2") {
+#     #AWS G3.4xLarge M60
+#     AudioInstall
+#   }
+#   ElseIF ($gputype.Substring(13, 8) -eq "DEV_118A") {
+#     #AWS G2.2xLarge K520
+#     AudioInstall
+#   }
+#   Elseif ($gputype.substring(13, 8) -eq "DEV_15F8") {
+#     #Tesla P100
+#     AudioInstall
+#   }
+#   Elseif ($gputype.substring(13, 8) -eq "DEV_1BB3") {
+#     #Tesla P4
+#     AudioInstall
+#   }
+#   Elseif ($gputype.substring(13, 8) -eq "DEV_1EB8") {
+#     #Tesla T4
+#     AudioInstall
+#   }
+#   Elseif ($gputype.substring(13, 8) -eq "DEV_1430") {
+#     #Quadro M2000
+#     AudioInstall
+#   }
+#   Elseif ($gputype.substring(13, 8) -eq "DEV_7362") {
+#     #AMD V520
+#     AudioInstall
+#   }
+#   Else {
+#   }
+# }
+
+#Audio Driver Install
+<#
+    (New-Object System.Net.WebClient).DownloadFile("http://rzr.to/surround-pc-download", "C:\ParsecTemp\Apps\razer-surround-driver.exe")
+    ExtractRazerAudio
+    ModidifyManifest
+    $OriginalLocation = Get-Location
+    Set-Location -Path 'C:\ParsecTemp\Apps\razer-surround-driver\$TEMP\RazerSurroundInstaller\'
+    Start-Process RzUpdateManager.exe
+    Set-Location $OriginalLocation
+    Set-Service -Name audiosrv -StartupType Automatic
+    #>
+# (New-Object System.Net.WebClient).DownloadFile("https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip", "C:\ParsecTemp\Apps\VBCable.zip")
+# New-Item -Path "C:\ParsecTemp\Apps\VBCable" -ItemType Directory | Out-Null
+# Expand-Archive -Path "C:\ParsecTemp\Apps\VBCable.zip" -DestinationPath "C:\ParsecTemp\Apps\VBCable"
+# $pathToCatFile = "C:\ParsecTemp\Apps\VBCable\vbaudio_cable64_win7.cat"
+# $FullCertificateExportPath = "C:\ParsecTemp\Apps\VBCable\VBCert.cer"
+# $VB = @{}
+# $VB.DriverFile = $pathToCatFile;
+# $VB.CertName = $FullCertificateExportPath;
+# $VB.ExportType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert;
+# $VB.Cert = (Get-AuthenticodeSignature -filepath $VB.DriverFile).SignerCertificate;
+# [System.IO.File]::WriteAllBytes($VB.CertName, $VB.Cert.Export($VB.ExportType))
+# Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher -FilePath $VB.CertName | Out-Null
+# Start-Process -FilePath "C:\ParsecTemp\Apps\VBCable\VBCABLE_Setup_x64.exe" -ArgumentList '-i', '-h'
+# Set-Service -Name audiosrv -
+
+# StartupType Automatic
+# Start-Service -Name audiosrv
+
+#Cleanup
+Remove-Item -Path C:\ParsecTemp\Drivers -force -Recurse
+Remove-Item -Path $path\ParsecTemp -force -Recurse
+remove-item "$env:AppData\Microsoft\Windows\Recent\*" -Recurse -Force | Out-Null
+
+# Attempts to read instance userdata and set up as Team Machine at startup
+$XML = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Description>Attempts to read instance userdata and set up as Team Machine at startup</Description>
+    <URI>\Setup Team Machine</URI>
+  </RegistrationInfo>
+  <Triggers>
+    <BootTrigger>
+      <Enabled>true</Enabled>
+    </BootTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>$(([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value)</UserId>
+      <LogonType>S4U</LogonType>
+      <RunLevel>HighestAvailable</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>true</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe</Command>
+      <Arguments>-file %programdata%\ParsecLoader\TeamMachineSetup.ps1</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
+
+try {
+  Get-ScheduledTask -TaskName "Setup Team Machine" -ErrorAction Stop | Out-Null
+  Unregister-ScheduledTask -TaskName "Setup Team Machine" -Confirm:$false
+}
+catch {}
+$action = New-ScheduledTaskAction -Execute 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-file %programdata%\ParsecLoader\TeamMachineSetup.ps1'
+$trigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -XML $XML -TaskName "Setup Team Machine" | Out-Null
