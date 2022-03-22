b058352cae2d38eabea8f9be34541ead982c3289
Daniel Hagen <daniel.b.hagen@gmail.com>
2022-03-22 16:32:25 -0700

    Missed one

--- AWSImageBuilder.ps1
+++ AWSImageBuilder.ps1
@@ -171,10 +171,8 @@ if ((gwmi win32_operatingsystem | % caption) -like '*Windows Server 2019*') {
 Write-Host "Final customization scripts"
 ### Customization for Remote use
 # Disable IE Security
-$RegistryPath = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-New-Item 
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
+Set-RegistryItem -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -Force
+# New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
 ## Stop-Process -Name Explorer -Force
 
 # Sets all applications to force close on shutdown
