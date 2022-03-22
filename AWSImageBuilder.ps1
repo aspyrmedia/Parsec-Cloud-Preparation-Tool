6b295f1b2daec997d022b6709b16fb831bbe4530
Daniel Hagen <daniel.b.hagen@gmail.com>
2022-03-22 13:48:58 -0700

    Don’t need to restart Explorer.exe

--- AWSImageBuilder.ps1
+++ AWSImageBuilder.ps1
@@ -129,7 +129,7 @@ Write-Host "Final customization scripts"
 # Disable IE Security
 Set-Itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -name IsInstalled -value 0 -force | Out-Null
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
-Stop-Process -Name Explorer -Force
+## Stop-Process -Name Explorer -Force
 
 # Sets all applications to force close on shutdown
 if (((Get-Item -Path "HKCU:\Control Panel\Desktop").GetValue("AutoEndTasks") -ne $null) -eq $true) {
